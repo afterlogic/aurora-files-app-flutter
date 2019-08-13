d8bfb532724564b207cfd6407158a712dc730bff
Vadim <vadeseg@gmail.com>
2019-08-13 14:49:49 +0300

    signed app for iOS

---
+++ lib/themimg/cupertino_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppMaterialTheme {
+  static final theme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Color(0xFF004166),
+    accentColor: Color(0xFF72bbe4),
+    errorColor: Colors.red[900],
+    inputDecorationTheme: _inputTheme,
+    buttonTheme: _buttonTheme,
+  );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.pink,
+    accentColor: Colors.blue,
+    errorColor: Colors.red[900],
+    inputDecorationTheme: _inputTheme,
+    buttonTheme: _buttonTheme,
+  );
+
+  static final _inputTheme = InputDecorationTheme(
+    border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+  );
+
+  static final _buttonTheme = ButtonThemeData(
+    shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(50.0)),
+    textTheme: ButtonTextTheme.primary,
+  );
+}
+

fbeb3b6feb29c6ebf689009ecbe09e6679ed689c
Vadim <vadeseg@gmail.com>
2019-11-11 13:55:33 +0300

    replaced splash with ripple

--- lib/themimg/material_theme.dart
+++ lib/themimg/material_theme.dart
@@ -14,6 +14,7 @@ class AppMaterialTheme {
     selectedRowColor: Color(0x11660041),
     highlightColor: Platform.isIOS ? Colors.transparent : null,
     splashColor: Platform.isIOS ? Colors.transparent : null,
+    splashFactory: InkRipple.splashFactory,
     inputDecorationTheme: _inputTheme,
     buttonTheme: _buttonTheme,
   );
@@ -29,6 +30,7 @@ class AppMaterialTheme {
     selectedRowColor: Color(0x11660041),
     highlightColor: Platform.isIOS ? Colors.transparent : null,
     splashColor: Platform.isIOS ? Colors.transparent : null,
+    splashFactory: InkRipple.splashFactory,
     inputDecorationTheme: _inputTheme,
     buttonTheme: _buttonTheme,
   );

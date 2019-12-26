fc749a0d862f5e63ce43c95c8e2bdfd6a8d2e602
Nikit Yakovlev <nikita.ya@webant.ru>
2019-12-26 10:37:18 +0300

    version 1.0.0 build 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+//import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -7,10 +7,10 @@ import 'modules/app_screen.dart';
 import 'utils/pgp_key_util.dart';
 
 void main() {
-  if (!kDebugMode) {
-    Crashlytics.instance.enableInDevMode = true;
-    FlutterError.onError = Crashlytics.instance.recordFlutterError;
-  }
+//  if (!kDebugMode) {
+//    Crashlytics.instance.enableInDevMode = true;
+//    FlutterError.onError = Crashlytics.instance.recordFlutterError;
+//  }
   DI.init();
   runApp(App());
 }

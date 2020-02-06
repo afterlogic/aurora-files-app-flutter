5e0c3d471a1537281e258bac286ab163c52dc3a9
NyAkovlev <princesakenny98@gmail.com>
2020-02-06 15:31:12 +0300

    ios crashlytics

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,14 +3,14 @@ import Flutter
 import CommonCrypto
 import Foundation
 import receive_sharing
-
+import Firebase
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-
+    FirebaseApp.configure()
     UserDefaults().set("group.privatemail.files",forKey: SwiftReceiveSharingPlugin.shareGroupKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)

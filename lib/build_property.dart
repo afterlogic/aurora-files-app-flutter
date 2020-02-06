962c0832acf061f01955111eb87e339383e1d23c
Nikita Yakovlev <princesakenny98@gmail.com>
2020-02-06 16:11:21 +0300

    add private mail google service

--- lib/build_property.dart
+++ lib/build_property.dart
@@ -1,18 +1,18 @@
 class BuildProperty {
-  static const icon = "build_res/afterlogic/image/icon.png";
+  static const icon = "build_res/private_mail/image/icon.png";
 
-  static const mainLogo = "build_res/afterlogic/image/main_logo.png";
+  static const mainLogo = "build_res/private_mail/image/main_logo.png";
 
   static const foregroundIcon =
-      "build_res/afterlogic/image/icon_foreground.png";
+      "build_res/private_mail/image/icon_foreground.png";
 
-  static const appName = "Aurora Files";
+  static const appName = "PrivateMail Files";
 
-  static const backgroundColor = "#75B8F3";
+  static const backgroundColor = "#34113C";
 
-  static const splashGradientTop = "#E18F84";
+  static const splashGradientTop = "#420324";
 
-  static const splashGradientBottom = "#43D0BF";
+  static const splashGradientBottom = "#252352";
 
-  static const secureSharingEnable = false;
+  static const secureSharingEnable = true;
 }
--- lib/modules/auth/screens/two_factor_auth.dart
+++ lib/modules/auth/screens/two_factor_auth.dart
@@ -6,9 +6,11 @@ import 'package:aurorafiles/modules/app_store.dart';
 import 'package:aurorafiles/modules/files/files_route.dart';
 import 'package:aurorafiles/override_platform.dart';
 import 'package:aurorafiles/shared_ui/app_button.dart';
+import 'package:aurorafiles/shared_ui/app_input.dart';
 import 'package:aurorafiles/shared_ui/main_gradient.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:theme/app_theme.dart';
 
 class TwoFactorAuth extends StatefulWidget {
   @override
@@ -28,72 +30,66 @@ class _TwoFactorAuthState extends State<TwoFactorAuth> {
     final theme = Theme.of(context);
     final mq = MediaQuery.of(context);
 
-    return Scaffold(
-      body: MainGradient(
-        child: SizedBox(
-          height: mq.size.height - mq.viewInsets.bottom,
-          width: mq.size.width,
-          child: Center(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: 22.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Hero(
-                    tag: "logo",
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                      child: Image.asset(BuildProperty.mainLogo),
-                    ),
-                  ),
-                  SizedBox(height: 70.0),
-                  Center(
-                    child: Text(
-                      s.two_factor_auth,
-                      textAlign: TextAlign.center,
-                      style: theme.textTheme.subhead.copyWith(
-                        fontSize: 20,
-                        color: Colors.white,
+    return Theme(
+      data: AppTheme.darkTheme,
+      child: Scaffold(
+        body: MainGradient(
+          child: SizedBox(
+            height: mq.size.height - mq.viewInsets.bottom,
+            width: mq.size.width,
+            child: Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 22.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Hero(
+                      tag: "logo",
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: Image.asset(BuildProperty.mainLogo),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 40.0),
-                  if (PlatformOverride.isIOS)
-                    CupertinoTextField(
+                    SizedBox(height: 70.0),
+                    Center(
+                      child: Text(
+                        s.two_factor_auth,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.subhead.copyWith(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    AppInput(
+                      labelText: s.pin,
                       style: TextStyle(color: Colors.white),
-                      cursorColor: Theme.of(context).accentColor,
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(color: Colors.white38))),
-                      placeholderStyle: TextStyle(color: Colors.white70),
-                      placeholder: s.pin,
+                      inputCase: InputCase.Underline,
                       controller: pinCtrl,
                     ),
-                  if (!PlatformOverride.isIOS)
-                    TextFormField(
-                      decoration: InputDecoration(labelText: s.pin),
-                      controller: pinCtrl,
+                    SizedBox(height: 10),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red),
                     ),
-                  SizedBox(height: 10),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red),
-                  ),
-                  SizedBox(height: 20.0),
-                  AppButton(
-                    isLoading: isProgress,
-                    buttonCase: ButtonCase.Filled,
-                    width: double.infinity,
-                    text: s.verify_pin,
-                    onPressed: checkCode,
-                  ),
-                  AppButton(
-                    buttonCase: ButtonCase.Cancel,
-                    width: double.infinity,
-                    text: s.cancel,
-                    onPressed: isProgress ? null : () => Navigator.pop(context),
-                  )
-                ],
+                    SizedBox(height: 20.0),
+                    AppButton(
+                      isLoading: isProgress,
+                      buttonCase: ButtonCase.Filled,
+                      width: double.infinity,
+                      text: s.verify_pin,
+                      onPressed: checkCode,
+                    ),
+                    AppButton(
+                      buttonCase: ButtonCase.Cancel,
+                      width: double.infinity,
+                      text: s.cancel,
+                      onPressed:
+                          isProgress ? null : () => Navigator.pop(context),
+                    )
+                  ],
+                ),
               ),
             ),
           ),

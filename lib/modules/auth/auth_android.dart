345634e9a2ec1c5b8468e893c014f8c61f54ff3a
Nikita Yakovlev <princesakenny98@gmail.com>
2020-08-18 14:21:01 +0300

    update dependency version

--- lib/modules/auth/auth_android.dart
+++ lib/modules/auth/auth_android.dart
@@ -46,7 +46,7 @@ class _AuthAndroidState extends State<AuthAndroid> {
     _authState.emailCtrl.text = _authState.userEmail;
     _authState.passwordCtrl.text = "";
     if (kDebugMode) {
-//      _authState.hostCtrl.text = AuthData.host;
+      _authState.hostCtrl.text = AuthData.host;
       _authState.emailCtrl.text = AuthData.email;
       _authState.passwordCtrl.text = AuthData.password;
     }
